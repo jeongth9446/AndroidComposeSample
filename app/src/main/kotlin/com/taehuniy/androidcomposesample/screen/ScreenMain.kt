c34c68efe07b44a034cca45842f29aa925667bd0
jeongth9446 <52663419+jeongth9446@users.noreply.github.com>
2023-01-21 17:42:01 +0900

    [#4, 오픈소스 라이선스 화면 구성]

--- app/src/main/kotlin/com/taehuniy/androidcomposesample/screen/ScreenMain.kt
+++ app/src/main/kotlin/com/taehuniy/androidcomposesample/screen/ScreenMain.kt
@@ -1,20 +1,31 @@
 package com.taehuniy.androidcomposesample.screen
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 @Composable
 fun ScreenMain(
     onNavigateToScreenVideo: () -> Unit
 ) {
+    val context = LocalContext.current
+
     Column {
         Text(text = "Android Compose Sample")
 
         // ScreenVideo
         Button(onClick = onNavigateToScreenVideo) {
             Text("Go To Video")
+        }
+        
+        // OssLicensesMenuActivity
+        // TODO : Move to Setting Menu
+        Button(onClick = { context.startActivity(Intent(context, OssLicensesMenuActivity::class.java)) }) {
+            Text("Go To Licenses")
         }
     }
 }
